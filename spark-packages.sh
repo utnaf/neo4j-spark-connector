ef5f8c3b5b0fe54e46d1303c9ac8768ac511bc17
Davide Fantuzzi <davide.fantuzzi@larus-ba.it>
2021-03-31 10:55:24 +0200

    update version and fixed spark-packages script

--- spark-packages.sh
+++ spark-packages.sh
@@ -1,10 +1,11 @@
 if [[ $# -lt 2 ]] ; then
-    echo "Usage ./spark-packages.sh <VERSION> <SCALA-VERSION>"
+    echo "Usage ./spark-packages.sh <VERSION> <SCALA-VERSION> <SPARK-VERSION>"
     exit 1
 fi
 
 ARTIFACT=neo4j-connector-apache-spark_$2
-VERSION=$1
+SPARK_VERSION=$3
+VERSION=$1_for_spark_$SPARK_VERSION
 ./mvnw clean install -Pscala-$2 -DskipTests
 cat << EOF > target/$ARTIFACT-$VERSION.pom
 <project>
@@ -15,6 +16,6 @@ cat << EOF > target/$ARTIFACT-$VERSION.pom
 </project>
 EOF
 cp pom.xml target/$ARTIFACT-$VERSION.pom
-cp target/$ARTIFACT-$VERSION.jar target/$ARTIFACT-$VERSION.jar
+cp spark-$SPARK_VERSION/target/$ARTIFACT-$VERSION.jar target/$ARTIFACT-$VERSION.jar
 zip -jv target/$ARTIFACT-$VERSION.zip target/$ARTIFACT-$VERSION.pom target/$ARTIFACT-$VERSION.jar
 xdg-open target
