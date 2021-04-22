8d4e157fac7f1e54a4b6d125a0f931bcab6725f4
Davide Fantuzzi <davide.fantuzzi@larus-ba.it>
2021-04-22 17:43:17 +0200

    improved test

--- spark-2.4/src/test/scala/org/neo4j/spark/DataSourceStreamingTSE.scala
+++ spark-2.4/src/test/scala/org/neo4j/spark/DataSourceStreamingTSE.scala
@@ -1,9 +1,11 @@
 package org.neo4j.spark
 
-import org.junit.Assert._
+import org.hamcrest.Matchers
 import org.junit.Test
+import org.neo4j.spark.Assert.ThrowingSupplier
 
 import java.util.UUID
+import java.util.concurrent.TimeUnit
 
 class DataSourceStreamingTSE extends SparkConnectorScalaBaseTSE {
 
@@ -12,22 +14,21 @@ class DataSourceStreamingTSE extends SparkConnectorScalaBaseTSE {
     val rateDf = ss.readStream.format("rate")
       .option("rowsPerSecond", 1)
       .load()
-
     val query = rateDf.writeStream.format(classOf[DataSource].getName)
       .option("url", SparkConnectorScalaSuiteIT.server.getBoltUrl)
       .option("labels", "Timestamp")
       .option("checkpointLocation", "/tmp/checkpoint/" + UUID.randomUUID().toString)
       .start()
 
-    query.awaitTermination(2000)
-    Thread.sleep(500)
+    Assert.assertEventually(new ThrowingSupplier[Boolean, Exception] {
+      override def get(): Boolean = {
+        val neo4jRateDf = ss.read.format(classOf[DataSource].getName)
+          .option("url", SparkConnectorScalaSuiteIT.server.getBoltUrl)
+          .option("labels", "Timestamp")
+          .load()
+        neo4jRateDf.collectAsList().size() > 0
+      }
+    }, Matchers.equalTo(true), 30L, TimeUnit.SECONDS)
     query.stop()
-
-    val neo4jRateDf = ss.read.format(classOf[DataSource].getName)
-      .option("url", SparkConnectorScalaSuiteIT.server.getBoltUrl)
-      .option("labels", "Timestamp")
-      .load()
-
-    assertTrue(neo4jRateDf.collectAsList().size() > 0)
   }
 }
