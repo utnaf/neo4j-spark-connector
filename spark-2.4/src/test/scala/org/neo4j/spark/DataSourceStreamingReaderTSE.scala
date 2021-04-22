91e064cdeee3e723f8e317d74cd07c441589d434
Davide Fantuzzi <davide.fantuzzi@larus-ba.it>
2021-04-22 09:03:10 +0200

    fixed #262: add support for source streaming

--- spark-2.4/src/test/scala/org/neo4j/spark/DataSourceStreamingReaderTSE.scala
+++ spark-2.4/src/test/scala/org/neo4j/spark/DataSourceStreamingReaderTSE.scala
@@ -1,36 +1,68 @@
 package org.neo4j.spark
 
-import org.junit.Assert.assertTrue
+import org.hamcrest.Matchers
 import org.junit.Test
 import org.neo4j.driver.summary.ResultSummary
 import org.neo4j.driver.{Transaction, TransactionWork}
+
+import java.util.concurrent.TimeUnit
 
 class DataSourceStreamingReaderTSE extends SparkConnectorScalaBaseTSE {
 
   @Test
   def testReadStream(): Unit = {
+    SparkConnectorScalaSuiteIT.session()
+      .writeTransaction(
+        new TransactionWork[ResultSummary] {
+          override def execute(tx: Transaction): ResultSummary = tx.run("CREATE (n:Movie {title: 'My movie 0'})").consume()
+        })
+
     val stream = ss.readStream.format(classOf[DataSource].getName)
       .option("url", SparkConnectorScalaSuiteIT.server.getBoltUrl)
       .option("labels", "Movie")
       .load()
 
     val query = stream.writeStream.format("console").start()
-
-    assertTrue(query.lastProgress == null)
-
-    query.awaitTermination(3000)
+    query.awaitTermination(2000)
 
     SparkConnectorScalaSuiteIT.session()
       .writeTransaction(
         new TransactionWork[ResultSummary] {
-          override def execute(tx: Transaction): ResultSummary = tx.run("CREATE (n:Movie {title: 'My movie'})").consume()
+          override def execute(tx: Transaction): ResultSummary = tx.run("CREATE (n:Movie {title: 'My movie 1'})").consume()
         })
 
+    Assert.assertEventually(new Assert.ThrowingSupplier[Boolean, Exception] {
+      override def get(): Boolean = {
+        query.lastProgress != null && query.lastProgress.numInputRows == 1
+      }
+    }, Matchers.equalTo(true), 3, TimeUnit.SECONDS)
+
     query.stop()
+  }
 
-    while(query.isActive) {  }
+  @Test
+  def testReadStreamWithNoValues(): Unit = {
+    SparkConnectorScalaSuiteIT.session()
+      .writeTransaction(
+        new TransactionWork[ResultSummary] {
+          override def execute(tx: Transaction): ResultSummary = tx.run("MATCH (n) DETACH DELETE n").consume()
+        })
 
-    assertTrue(query.lastProgress.numInputRows == 1)
+    val stream = ss.readStream.format(classOf[DataSource].getName)
+      .option("url", SparkConnectorScalaSuiteIT.server.getBoltUrl)
+      .option("labels", "Movie")
+      .load()
+
+    val query = stream.writeStream.format("console").start()
+    query.awaitTermination(2000)
+
+    Assert.assertEventually(new Assert.ThrowingSupplier[Boolean, Exception] {
+      override def get(): Boolean = {
+        query.lastProgress != null && query.lastProgress.numInputRows == 0
+      }
+    }, Matchers.equalTo(true), 3, TimeUnit.SECONDS)
+
+    query.stop()
   }
 }
 
