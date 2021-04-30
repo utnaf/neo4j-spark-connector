0ec9f64ac81432b335cd4bc3648115632c283334
Davide Fantuzzi <davide.fantuzzi@larus-ba.it>
2021-04-30 09:56:08 +0200

    add test for source with relationship and query

--- spark-2.4/src/test/scala/org/neo4j/spark/DataSourceStreamingReaderTSE.scala
+++ spark-2.4/src/test/scala/org/neo4j/spark/DataSourceStreamingReaderTSE.scala
@@ -4,9 +4,8 @@ import org.apache.spark.sql.streaming.StreamingQuery
 import org.hamcrest.Matchers
 import org.junit.{After, Test}
 import org.neo4j.driver.summary.ResultSummary
-import org.neo4j.driver.{Transaction, TransactionWork, Values}
+import org.neo4j.driver.{Transaction, TransactionWork}
 
-import java.util.Collections
 import java.util.concurrent.{Executors, TimeUnit}
 import scala.collection.mutable
 
@@ -22,7 +21,7 @@ class DataSourceStreamingReaderTSE extends SparkConnectorScalaBaseTSE {
   }
 
   @Test
-  def testReadStream(): Unit = {
+  def testReadStreamWithLabels(): Unit = {
     SparkConnectorScalaSuiteIT.session()
       .writeTransaction(
         new TransactionWork[ResultSummary] {
@@ -74,7 +73,136 @@ class DataSourceStreamingReaderTSE extends SparkConnectorScalaBaseTSE {
         }
         actual.toList == expected.toList
       }
-    }, Matchers.equalTo(true), 10, TimeUnit.SECONDS)
+    }, Matchers.equalTo(true), 30, TimeUnit.SECONDS)
+  }
+
+  @Test
+  def testReadStreamWithRelationship(): Unit = {
+    SparkConnectorScalaSuiteIT.session()
+      .writeTransaction(
+        new TransactionWork[ResultSummary] {
+          override def execute(tx: Transaction): ResultSummary = tx.run(
+            """
+              |CREATE (person:Person) SET person.age = 0
+              |CREATE (post:Post) SET post.hash = "hash0"
+              |CREATE (person)-[:LIKES]->(post)
+              |""".stripMargin).consume()
+        })
+
+    val stream = ss.readStream.format(classOf[DataSource].getName)
+      .option("url", SparkConnectorScalaSuiteIT.server.getBoltUrl)
+      .option("relationship", "LIKES")
+      .option("relationship.source.labels", "Person")
+      .option("relationship.target.labels", "Post")
+      .load()
+
+    query = stream.writeStream
+      .format("memory")
+      .queryName("testReadStream")
+      .start()
+
+    val total = 10
+    Executors.newSingleThreadExecutor().submit(new Runnable {
+      override def run(): Unit = {
+        (1 to total).foreach(index => {
+          SparkConnectorScalaSuiteIT.session()
+            .writeTransaction(new TransactionWork[ResultSummary] {
+              override def execute(tx: Transaction): ResultSummary = {
+                tx.run(
+                  s"""
+                    |CREATE (person:Person) SET person.age = $index
+                    |CREATE (post:Post) SET post.hash = "hash$index"
+                    |CREATE (person)-[:LIKES]->(post)
+                    |""".stripMargin)
+                  .consume()
+              }
+            })
+          Thread.sleep(100)
+        })
+      }
+    })
+
+    val expected = (0 to total).map(index => Map(
+      "<rel.type>" -> "LIKES",
+      "<source.labels>"-> mutable.WrappedArray.make(Array("Person")),
+      "source.age" -> index,
+      "<target.labels>"-> mutable.WrappedArray.make(Array("Post")),
+      "target.hash" -> s"hash$index"
+    ))
+
+    Assert.assertEventually(new Assert.ThrowingSupplier[Boolean, Exception] {
+      override def get(): Boolean = {
+        val df = ss.sql("select * from testReadStream order by `source.age`")
+        val collect = df.collect()
+        val actual: Array[Map[String, Any]] = if (!df.columns.contains("source.age") || !df.columns.contains("target.hash")) {
+          Array.empty
+        } else {
+          collect.map(row => Map(
+            "<rel.type>" -> row.getAs[java.util.List[String]]("<rel.type>"),
+            "<source.labels>"-> row.getAs[java.util.List[String]]("<source.labels>"),
+            "source.age" -> row.getAs[java.util.List[String]]("source.age"),
+            "<target.labels>"-> row.getAs[java.util.List[String]]("<target.labels>"),
+            "target.hash" -> row.getAs[java.util.List[String]]("target.hash")
+          ))
+        }
+        actual.toList == expected.toList
+      }
+    }, Matchers.equalTo(true), 30, TimeUnit.SECONDS)
+  }
+
+
+  @Test
+  def testReadStreamWithQuery(): Unit = {
+    SparkConnectorScalaSuiteIT.session()
+      .writeTransaction(new TransactionWork[ResultSummary] {
+        override def execute(tx: Transaction): ResultSummary = {
+          tx.run(s"CREATE (person:Person) SET person.age = 0")
+            .consume()
+        }
+      })
+
+    val stream = ss.readStream.format(classOf[DataSource].getName)
+      .option("url", SparkConnectorScalaSuiteIT.server.getBoltUrl)
+      .option("query", "MATCH (p:Person) RETURN p.age AS age")
+      .load()
+
+    query = stream.writeStream
+      .format("memory")
+      .queryName("testReadStream")
+      .start()
+
+    val total = 10
+    Executors.newSingleThreadExecutor().submit(new Runnable {
+      override def run(): Unit = {
+        (1 to total).foreach(index => {
+          SparkConnectorScalaSuiteIT.session()
+            .writeTransaction(new TransactionWork[ResultSummary] {
+              override def execute(tx: Transaction): ResultSummary = {
+                tx.run(s"CREATE (person:Person) SET person.age = $index")
+                  .consume()
+              }
+            })
+          Thread.sleep(100)
+        })
+      }
+    })
+
+    val expected = (0 to total).map(index => Map("age" -> index))
+
+    Assert.assertEventually(new Assert.ThrowingSupplier[Boolean, Exception] {
+      override def get(): Boolean = {
+        val df = ss.sql("select * from testReadStream order by age")
+        val collect = df.collect()
+        val actual: Array[Map[String, Any]] = if (!df.columns.contains("age")) {
+          Array.empty
+        } else {
+          collect.map(row => Map(
+            "age" -> row.getAs[java.util.List[String]]("age"),
+          ))
+        }
+        actual.toList == expected.toList
+      }
+    }, Matchers.equalTo(true), 30, TimeUnit.SECONDS)
   }
 }
 
